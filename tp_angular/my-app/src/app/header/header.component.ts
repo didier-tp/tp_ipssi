5e43a41a3036d45e215d467afd6e651b0f007f3a
didier <didier@d-defrance.fr>
2019-08-06 11:24:44 +0200

    avec @Input et @Output

--- tp_angular/my-app/src/app/header/header.component.ts
+++ tp_angular/my-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
+
+  @Output()
+  couleurEvent : EventEmitter<{value:string}> = new EventEmitter<{value:string}>();
+
+  riseEvent(evt:any){
+    //this.couleurEvent.emit({value: evt.target.value});
+    this.couleurEvent.emit({value: this.couleurFond});
+  }
+
+  @Input()
+  titre : string = "titre par defaut" ;
 
   listeCouleurs = [ "lightgrey" ,"red" , "green" , "blue"]
   couleurFond : string = "lightgrey";
